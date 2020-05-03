7616a42eb74cc0027eef58f891576613f59e1119
Neel Pai <me@nileshpai.com>
2020-05-02 20:54:03 -0500

    added the donate button

--- js/main.js
+++ js/main.js
@@ -1,3 +1,24 @@
-$(window).scroll(function () {
+window.onscroll = function () {
+    navTranstition()
+    growShrinkLogo()
+}
+
+function navTranstition() {
     $('nav').toggleClass('scrolled', $(this).scrollTop() > 50);
-});
+}
+
+var Logo = document.getElementById("Logo");
+var endOfDocumentTop = 150;
+var size = 0;
+
+function growShrinkLogo() {
+    var scroll = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
+
+    if (size == 0 && scroll > endOfDocumentTop) {
+        Logo.className = 'smallLogo';
+        size = 1;
+    } else if (size == 1 && scroll <= endOfDocumentTop) {
+        Logo.className = 'largeLogo';
+        size = 0;
+    }
+}
