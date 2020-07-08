d71852fee73186301ccf4a165de1990c8a1ddbf8
sameerp815 <62110582+sameerp815@users.noreply.github.com>
2020-07-08 09:36:23 -0500

    update to schema

--- backend/Models/learnerModel.js
+++ backend/Models/learnerModel.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 const Schema = mongoose.Schema;
 
-export const PlayerSchema = new Schema({
+export const learnerSchema = new Schema({
     userName: {
         type: String,
         required: true
