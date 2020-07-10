d78845dfc9d5006885987a8f4fcc8f31675999ac
sameerp815 <62110582+sameerp815@users.noreply.github.com>
2020-07-09 21:56:16 -0500

    teaching stuff

--- backend/Controllers/learnerController.js
+++ backend/Controllers/learnerController.js
@@ -3,6 +3,11 @@ import {learnerSchema} from '../Models/learnerModel';
 
 const Learner = mongoose.model('Learner', learnerSchema);
 
+
+
+
+
+
 export const addNewLearner = (req,res) => {
 
     let newLearner = new Learner(req.body);
