5a6b15b961ecf3e58371c70c7a7cd12a0d3e13cc
rishixyz9 <rishabh@ev3everywhere.com>
2020-09-17 18:31:36 -0500

    trying js for first time

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ const hamburger = document.querySelector(".hamburger");
 const navLinks = document.querySelector(".nav-links");
 const links = document.querySelectorAll(".nav-links li");
 const spacer = document.querySelector(".spacer");
-
+var toHide = document.querySelectorAll("#viewdesk");
+var count = 0;
 
 hamburger.addEventListener('click', () => {
     navLinks.classList.toggle("open");
@@ -10,4 +11,12 @@ hamburger.addEventListener('click', () => {
     links.forEach(link => {
         link.classList.toggle("fade");
     });
+    if (count%2 == 1){
+        count++;
+        toHide[0].classList.remove('hidden');
+    }else {
+        toHide[0].classList.add('hidden');
+        count++;
+    }
+    console.log(toHide[0].classList);
 });
