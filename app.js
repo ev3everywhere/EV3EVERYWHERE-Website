edb0b861548118b57248980d769ade6dd339905e
sameerp815 <62110582+sameerp815@users.noreply.github.com>
2020-09-17 19:13:52 -0500

    animation for navbar fixes

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const hamburger = document.querySelector(".hamburger");
 const navLinks = document.querySelector(".nav-links");
 const links = document.querySelectorAll(".nav-links li");
-const spacer = document.querySelector(".spacer");
+const hidden = document.querySelector(".hidden");
 
 
 hamburger.addEventListener('click', () => {
     navLinks.classList.toggle("open");
-    spacer.classList.toggle("open");
+    hidden.classList.toggle("open");
     links.forEach(link => {
         link.classList.toggle("fade");
     });
