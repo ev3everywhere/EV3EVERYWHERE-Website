1275fad12ec0056e48cfa777619f01e4c6aa7904
sameerp815 <62110582+sameerp815@users.noreply.github.com>
2020-09-17 19:05:25 -0500

    Revert "trying js for first time"
    This reverts commit 5a6b15b961ecf3e58371c70c7a7cd12a0d3e13cc.

--- app.js
+++ app.js
@@ -2,8 +2,7 @@ const hamburger = document.querySelector(".hamburger");
 const navLinks = document.querySelector(".nav-links");
 const links = document.querySelectorAll(".nav-links li");
 const spacer = document.querySelector(".spacer");
-var toHide = document.querySelectorAll("#viewdesk");
-var count = 0;
+
 
 hamburger.addEventListener('click', () => {
     navLinks.classList.toggle("open");
@@ -11,12 +10,4 @@ hamburger.addEventListener('click', () => {
     links.forEach(link => {
         link.classList.toggle("fade");
     });
-    if (count%2 == 1){
-        count++;
-        toHide[0].classList.remove('hidden');
-    }else {
-        toHide[0].classList.add('hidden');
-        count++;
-    }
-    console.log(toHide[0].classList);
 });
